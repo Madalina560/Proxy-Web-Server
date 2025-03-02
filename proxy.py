4f0f9599f5ca7f8491e63e0e39459d77481b873b
MadalinaC560 <costovicimadalina@gmail.com>
2025-03-02 16:17:16 +0000

    Added HTTP serving functionallity

--- proxy.py
+++ proxy.py
@@ -1,12 +1,40 @@
 import socket
 
-# socket programmed acording to: https://www.youtube.com/watch?v=JNzfG7XMYSg
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 6040))
-s.listen(5)
+serverPort = 6040
 
-while True:
-    clientSocket, address = s.accept()
-    print(f"Connecion established from address {address}")
-    clientSocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientSocket.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 6040))
+s.listen(5)
+print("Server ready to recieve")
+
+while True: 
+    #Establish the connection 
+    print('Ready to serve...') 
+    connectionSocket, addr = s.accept()
+    print(f"Connection established with {addr}")
+    try: 
+        message =  connectionSocket.recv(1024).decode()
+        print("Recieved request:", message)
+
+        filename = message.split()[1][1:]                  
+        print(f"Client requested file: {filename}")                    
+        
+        with open(filename, "rb") as f:
+            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+            connectionSocket.send("Content type: text/html\r\n\r\n".encode())
+
+            chunk = f.read()
+            while chunk:
+                connectionSocket.send(chunk)
+                chunk = f.read(1024)
+
+    except FileNotFoundError: 
+        error_message = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found"
+        connectionSocket.send(error_message.encode())
+    
+    except Exception as e:
+        print("Error:", e)
+    
+    finally:
+        connectionSocket.close()
+                     
